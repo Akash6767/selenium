b308d568891e517554dda503d2d8d71b33e6d6cf
Akash Kamble <kambleakash679@gmail.com>
2022-09-25 21:30:30 +0530

    first commit

---
+++ src/BasicProgramme/Programme1.java
@@ -0,0 +1,13 @@
+package BasicProgramme;
+
+public class Programme1 
+
+	public static void main(String[] args 
+	{
+	System.out.println("welcome to the velocity");
+	System.out.println("it is brand of IT");
+		// TODO Auto-generated method stub
+
+	}
+
+}
